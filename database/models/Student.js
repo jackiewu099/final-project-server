0a4f07a7699b03ded9c67d86599b7383c38b8669
Jia Jun Wu <jiajunwu041@gmail.com>
2025-11-03 23:53:41 -0500

    Finished student model

--- database/models/Student.js
+++ database/models/Student.js
@@ -7,16 +7,46 @@ const Sequelize = require('sequelize');  // Import Sequelize
 const db = require('../db');  // Import Sequelize database instance called "db"
 
 const Student = db.define("student", {
-  firstname: {
+  firstName: {
     type: Sequelize.STRING,
-    allowNull: false
+    allowNull: false,
+    validate: {
+      notEmpty: true, // prevents empty string
+    },
   },
 
-  lastname: {
+  lastName: {
     type: Sequelize.STRING,
-    allowNull: false
-  }
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      notEmpty: true,
+      isEmail: true, // built-in Sequelize email validation
+    },
+  },
+
+  imageUrl: {
+    type: Sequelize.STRING,
+    allowNull: true,
+    defaultValue: "https://cdn-icons-png.flaticon.com/512/3135/3135715.png",
+  },
+
+  gpa: {
+    type: Sequelize.DECIMAL(3, 2), // e.g. 3.75
+    allowNull: true,
+    validate: {
+      min: 0.0,
+      max: 4.0,
+    },
+  },
 });
 
-// Export the student model
 module.exports = Student;
